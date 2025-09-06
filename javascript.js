b604c12f01602b5c730f4b75204df31194a05c4f
resetreboot1 <206232704+resetreboot1@users.noreply.github.com>
2025-09-06 22:00:51 +0600

    made the button initiate specific events to call  a function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,22 @@
 let boxContainer = document.querySelector(".box-container");
+let choiceButton = document.querySelector(".choice-button");
+let gridValue;
 let box;
+
+function assignGrids() {
+  if (gridValue < 1) {
+    return alert("The minimum number of grids is 1.")
+  }
+
+  if (gridValue > 100) {
+    return alert("The maximum number of grids is 100.")
+  }
+
+  if (gridValue > 0 && gridValue < 101){
+    
+  }
+}
+
 for (i = 0; i < 256; i++) {
   box = document.createElement("div")
   box.classList.add("box")
@@ -9,4 +26,10 @@ for (i = 0; i < 256; i++) {
 boxContainer.addEventListener("mouseover", (e) => {
   if (e.target.classList.contains("box"))
   e.target.style.backgroundColor = "black" 
+})
+
+choiceButton.addEventListener("click", (e) => {
+  gridValue = prompt("Choose a number between 1-100", "16")
+  
+  assignGrids()
 })
